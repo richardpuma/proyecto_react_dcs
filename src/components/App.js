645a15884715b323563d1c3aa6a11d9042a83b6b
richard pumapillo <richardpumac@gmail.com>
2019-12-02 23:20:15 -0500

    se agrega y configura react-bootstrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button'
 
 const App=()=>(
-  <h1>
-    prueba h1
-  </h1>
+  <>
+    <h1>
+      prueba h1
+    </h1>
+    <Button variant="primary">Primary</Button>
+  </>
+  
 )
 
 export default App;
