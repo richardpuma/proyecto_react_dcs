14c73b4dc73e2f478a6525c15ca874077546e79b
richard pumapillo <richardpumac@gmail.com>
2019-12-04 18:08:02 -0500

    configuracion de rutas publicas y privadas

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Home from "./Pages/Home"
+import Protected from './Routes/Protected';
+import Products from "./Pages/Products"
+import Product from './Pages/Product';
+import Login from './Pages/Login';
+import Public from './Routes/Public'
+import Register from './Pages/Register'
+import Page404 from './Pages/Page404'
 
 const App=()=>(
   <Router>
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Protected path="/" exact component={Home} />      
+      <Protected path="/products" exact component={Products} />
+      <Protected path="/product/:id" component={Product} />
+
+      <Public path="/login" exact component={Login} />
+      <Public path="/register" exact component={Register} />
+
+      <Route component={Page404} />
     </Switch>
   </Router>
 )
---
+++ src/components/Routes/Public.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+const Public = ({ component: Component, ...rest }) => {
+  const userLogged = true
+  if (userLogged) {
+    return <Redirect to="/" />
+  }
+
+  return <Route {...rest} render={Component} />
+
+}
+
+export default Public;
